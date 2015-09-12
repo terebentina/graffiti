24865c2bf040d0d690a4fdac8aa03eb563d09452
Dan Caragea <dancaragea@gmail.com>
2015-09-12 17:56:45 +0300

    add support for restify server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 var koa = require('./koa');
 var hapi = require('./hapi');
 var express = require('./express');
+var express = require('./restify');
 
 module.exports.koa = koa.create();
 module.exports.hapi = hapi.create();
 module.exports.express = express.create();
+module.exports.restify = restify.create();
